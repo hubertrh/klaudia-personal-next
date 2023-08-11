933bc50238d4ae8c15736b99e290fb5fa7b5f097
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-08-11 17:42:45 +0100

    Add theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#5D2187',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
