3b255a1ccbdd07115b96dedb12c1b6f343cfac8f
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-08-14 23:29:35 +0100

    Add Sanity to Hero section

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,15 +1,22 @@
 import klaudiaHeroImage from '@/public/images/klaudia-hero.jpg';
+import { getHeroEntry } from '@/sanity/sanity-utils';
+import { HeroEntry } from '@/types/HeroEntry';
 import Image from 'next/image';
 
-export default function Hero() {
+export default async function Hero() {
+  const heroEntry: HeroEntry | null = await getHeroEntry();
+
   return (
     <section className="grid h-screen place-items-center">
       <Image
         className="h-min max-h-[60vh] w-min max-w-[60vw] translate-y-5 object-contain"
-        src={klaudiaHeroImage}
-        alt="Main picture of Klaudia"
+        src={heroEntry?.heroImage.url || klaudiaHeroImage}
+        alt={heroEntry?.heroImage.alt || 'Main picture of Klaudia'}
+        width={heroEntry?.heroImage.metadata.dimensions.width}
+        height={heroEntry?.heroImage.metadata.dimensions.height}
         fill={false}
         placeholder="blur"
+        blurDataURL={heroEntry?.heroImage.metadata.lqip}
         loading="eager"
         priority
       />
