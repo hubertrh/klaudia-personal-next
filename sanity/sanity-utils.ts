6bc42e37ae21a6fcd0b9f3d8cdf87c2f8a55e6c7
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-08-14 23:27:43 +0100

    Add query functions

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,7 @@
 import { createClient, groq } from 'next-sanity';
-import { Entry } from '@/types/Entry';
-import { PortfolioCategory } from '@/types/PortfolioCategory';
+import { AboutEntry } from '@/types/AboutEntry';
+import { ContactEntry } from '@/types/ContactEntry';
+import { HeroEntry } from '@/types/HeroEntry';
 
 const clientConfig = {
   projectId: 'ja4beydf',
@@ -8,60 +9,76 @@ const clientConfig = {
   apiVersion: '2023-08-07',
 };
 
-export async function getPortfolioCategories(): Promise<PortfolioCategory> {
+export async function getHeroEntry(): Promise<HeroEntry | null> {
   const client = createClient(clientConfig);
 
-  return client.fetch(
-    groq`*[_type == "category"]{
-      _id,
-      title,
-      featuredPicture{
-        asset->{
-          _id,
-          url
+  const query = groq`*[_type == "hero"] {
+    _id,
+    "heroImage": heroImage.asset->{
+      url,
+      alt,
+      metadata {
+        lqip,
+        dimensions {
+          width,
+          height
         }
-      },
-      entries[]{
-        _id,
-        name,
-        slug,
-        featuredPicture{
-          asset->{
-            _id,
-            url
-          }
-        },
-        pictures {
-          asset->{
-            _id,
-            url
-          }
-        }[],
       }
-    }`,
-  );
+    }
+  }[0]`;
+
+  try {
+    const result: HeroEntry = await client.fetch(query);
+    return result;
+  } catch (error) {
+    console.error('Failed to fetch hero entry:', error);
+    return null;
+  }
 }
 
-export async function getEntries(): Promise<Entry[]> {
+export async function getAboutEntry(): Promise<AboutEntry | null> {
   const client = createClient(clientConfig);
 
-  return client.fetch(
-    groq`*[_type == "entry"]{
-      _id,
-      name,
-      "slug": slug.current,
-      featuredPicture{
-        asset->{
-          _id,
-          url
-        }
-      },
-      pictures[]{
-        asset->{
-          _id,
-          url
+  const query = groq`*[_type == "about"] {
+    _id,
+    aboutMeTitle,
+    aboutMeDescription,
+    "aboutMeImage": aboutMeImage.asset->{
+      url,
+      alt,
+      metadata {
+        lqip,
+        dimensions {
+          width,
+          height
         }
       }
-    }`,
-  );
+    }
+  }[0]`;
+
+  try {
+    const result: AboutEntry = await client.fetch(query);
+    return result;
+  } catch (error) {
+    console.error('Failed to fetch about entry:', error);
+    return null;
+  }
+}
+
+export async function getContactEntry(): Promise<ContactEntry | null> {
+  const client = createClient(clientConfig);
+
+  const query = groq`*[_type == "contact"] {
+    _id,
+    contactMeTitle,
+    contactMeDescription,
+  }[0]`;
+
+  try {
+    const result: ContactEntry = await client.fetch(query);
+    return result;
+  } catch (error) {
+    console.error('Failed to fetch contact entry:', error);
+    return null;
+  }
 }
