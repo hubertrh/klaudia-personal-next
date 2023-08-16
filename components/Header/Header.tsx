2207e8047570584fdcc81c9777c98ab007d619a3
Hubert Rogala-Haracz <hubert.haracz@gmail.com>
2023-08-16 18:38:02 +0100

    Fix positioning of header items

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import SocialIcons from '../Common/SocialIcons';
 
 export default function header() {
   return (
-    <header className="fixed top-0 z-10 flex w-full max-w-screen-xl justify-between bg-zinc-50 px-12">
+    <header className="fixed top-0 z-10 flex w-full max-w-screen-xl items-center justify-between bg-zinc-50 px-4 py-3 sm:px-12 sm:py-0">
       <Navigation />
       <SocialIcons />
     </header>
